896c66a15a2fe90324287dac509aa0444e0024d6
Christian Gruber <christian.gruber@web.de>
2010-10-09 01:33:39 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Micropay::Application.config.secret_token = '3954f63611e2516170da3c1b26c4a3b83194cb07867cfd730712df34775463526b23bb3ed620cc026ac3b1da98c80c07f081e1570dcacd5258258661b5824d28'
