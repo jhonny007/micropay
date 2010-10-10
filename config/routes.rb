d962da1670fa18055c0d10f7bdd7d6f3c4d7b4e1
Christian Gruber <christian.gruber@web.de>
2010-10-10 17:33:14 +0200

    Removed default Rails page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Micropay::Application.routes.draw do
-  get "pages/home"
-  get "pages/contact"
-  get "pages/about"
-  get "pages/help"
+
+  match '/contact', :to => 'pages#contact'
+  match '/about',   :to => 'pages#about'
+  match '/help',    :to => 'pages#help'
+
+  root :to => "pages#home"
 
   resources :microposts
 
@@ -57,9 +59,7 @@ Micropay::Application.routes.draw do
   #     resources :products
   #   end
 
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+
 
   # See how all your routes lay out with "rake routes"
 
