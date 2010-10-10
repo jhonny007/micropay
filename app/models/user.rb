eada980b705d525476715e02f08a1113a84dbd47
Christian Gruber <christian.gruber@web.de>
2010-10-10 06:12:10 +0200

    CSS blueprint added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,15 @@
 class User < ActiveRecord::Base
   has_many :microposts
   has_one  :electronic_purse
+
+  attr_accessor :name, :email
+
+  def initialize(attributes = {})
+    @name  = attributes[:name]
+    @email = attributes[:email]
+  end
+
+  def formatted_email
+    "#{@name} <#{@email}>"
+  end
 end
