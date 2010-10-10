3ab3f006f1f6323e38cf4ee60c48cf997fb5a795
Christian Gruber <christian.gruber@web.de>
2010-10-10 23:57:16 +0200

    ran annotate

--- app/models/electronic_purse.rb
+++ app/models/electronic_purse.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+# Schema version: 20101009115355
+#
+# Table name: electronic_purses
+#
+#  id         :integer         not null, primary key
+#  credit     :decimal(, )
+#  user_id    :integer
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class ElectronicPurse < ActiveRecord::Base
   belongs_to :user
 end
