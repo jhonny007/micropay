664ecb4904193fa5682f2c9f49cb7ee5f36e7616
Christian Gruber <christian.gruber@web.de>
2010-10-10 18:52:07 +0200

    test the links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,9 @@
 module ApplicationHelper
 
+  def logo
+    image_tag("logo.png", :alt => "MicroPayment", :class => "roun")
+  end
+  
   # Return a title on a per-page basis.
   def title
     base_title = "MicroPayment"
