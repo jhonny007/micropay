ce9acc575ca64ab0bae1a78c051df42b8404e4b8
Christian Gruber <christian.gruber@web.de>
2010-10-11 14:07:14 +0200

    User model with passwords, and user show page

--- Gemfile
+++ Gemfile
@@ -34,4 +34,5 @@ end
 group :test do
   gem 'rspec', '2.0.0.rc'
   gem 'webrat', '0.7.1'
+  gem 'factory_girl_rails', '1.0'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101009115355) do
+ActiveRecord::Schema.define(:version => 20101011002129) do
 
   create_table "electronic_purses", :force => true do |t|
     t.decimal  "credit"
@@ -31,6 +31,10 @@ ActiveRecord::Schema.define(:version => 20101009115355) do
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "encrypted_password"
+    t.string   "salt"
   end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
 
 end
